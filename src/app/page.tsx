795eb501fa9e2520a395a9c8ca836ea3be8da03b
Kairrinn <benbrahimadil13@gmail.com>
2026-03-22 03:33:27 +0100

    Make active listings refresh dynamically

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
+
 import { ListingCard } from "@/components/listing-card";
 import { listListings } from "@/lib/services/listings-service";
 
